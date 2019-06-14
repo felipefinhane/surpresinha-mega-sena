f0c0c6ee63e0b128d9dc0ba0c48c1a3399fe9666
Felipe Finhane de Paula <felipefinhane@gmail.com>
2019-06-14 15:17:21 -0300

    feat: Create script for show luck numbers

---
+++ surpresinha.py
@@ -0,0 +1,32 @@
+import random
+
+def sortLuckNumber(arrList):
+    lNumber = random.randint(1,60)
+    if lNumber in arrList:
+        return sortLuckNumber(arrList)
+    return lNumber
+
+question02 = eval(raw_input('Quantidade de jogos: '))
+question01 = eval(raw_input('Quantidade de numeros: '))
+
+indiceGames = 0
+arrGames = []
+while(indiceGames < (question02 * 12 * 100)):
+    indiceGames += 1
+    indiceNumbers = 0
+    arrList = []
+    while(indiceNumbers < question01):
+        indiceNumbers += 1
+        luckNumber = sortLuckNumber(arrList)
+        arrList.append(luckNumber)
+    arrList.sort()
+    arrGames.append(arrList)
+
+random.shuffle(arrGames)
+contador = 0
+
+for game in arrGames:
+    if (contador >= question02):
+        break
+    contador += 1
+    print(game)
